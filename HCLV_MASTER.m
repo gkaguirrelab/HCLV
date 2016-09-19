a64ca86115f406089fbdbb040725b32c52349ccc
bock42 <abock@mac-pro.uphs.upenn.edu>
2016-09-19 09:54:07 -0400

    HCLV_MASTER - updates

--- HCLV_MASTER.m
+++ HCLV_MASTER.m
@@ -4,77 +4,127 @@
 %   Written by Andrew S Bock Sep 2016
 
 %% TOME_3001 - session 1
-session_dir = '/data/jag/TOME/TOME_3001/081916a';
-subject_name = 'TOME_3001'; % Freesurfer subject name (may not match job_name)
-outDir = fullfile(session_dir,'preprocessing_scripts');
-logDir = '/data/jet/abock/LOGS';
-job_name = 'TOME_3001'; % Name for this job/session (may not match subject_name)
-numRuns = 4; % number of bold runs
-reconall = 1;
-slicetiming = 0; % correct slice timings
-refvol = 1; % motion correct to 1st TR
-filtType = 'high';
-lowHz = 0.01;
-highHz = 0.10;
-physio = 1;
-motion = 1;
-task = 0;
-localWM = 1;
-anat = 1;
-amem = 20;
-fmem = 50;
-
-create_preprocessing_scripts(session_dir,subject_name,outDir,logDir,job_name...
-    ,numRuns,reconall,slicetiming,refvol,filtType,lowHz,highHz,physio,motion,task,localWM,anat,amem,fmem)
+params.sessionDir = '/data/jag/TOME/TOME_3001/081916a';
+params.subjectName = 'TOME_3001';
+params.outDir = fullfile(params.sessionDir,'preprocessing_scripts');
+params.logDir = '/data/jet/abock/LOGS';
+params.jobName = params.subjectName;
+params.numRuns = 4; 
+params.reconall = 0; % already ran once
+params.slicetiming = 0;
+params.refvol = 1;
+params.regFirst = 1;
+params.filtType = 'high';
+params.lowHz = 0.01;
+params.highHz = 0.10;
+params.physio = 1;
+params.motion = 1;
+params.task = 0;
+params.localWM = 1;
+params.anat = 1;
+params.amem = 20;
+params.fmem = 50;
+create_preprocessing_scripts(params);
 %% TOME_3001 - session 2
 
-%%% copy over the MPRAGE directory from session 1 %%%
+%%% copy over the MPRAGE directory from TOME_3001 - session 1 %%%
 
-session_dir = '/data/jag/TOME/TOME_3001/081916b';
-subject_name = 'TOME_3001'; % Freesurfer subject name (may not match job_name)
-outDir = fullfile(session_dir,'preprocessing_scripts');
-logDir = '/data/jet/abock/LOGS';
-job_name = 'TOME_3001'; % Name for this job/session (may not match subject_name)
-numRuns = 10; % number of bold runs
-reconall = 0;
-slicetiming = 0; % correct slice timings
-refvol = 1; % motion correct to 1st TR
-filtType = 'high';
-lowHz = 0.01;
-highHz = 0.10;
-physio = 1;
-motion = 1;
-task = 0;
-localWM = 1;
-anat = 1;
-amem = 20;
-fmem = 50;
-% Create preprocessing scripts
-create_preprocessing_scripts(session_dir,subject_name,outDir,logDir,job_name...
-    ,numRuns,reconall,slicetiming,refvol,filtType,lowHz,highHz,physio,motion,task,localWM,anat,amem,fmem);
-% project template
-project_template(session_dir,subject_name);
-
+params.sessionDir = '/data/jag/TOME/TOME_3001/081916b';
+params.subjectName = 'TOME_3001';
+params.outDir = fullfile(params.sessionDir,'preprocessing_scripts');
+params.logDir = '/data/jet/abock/LOGS';
+params.jobName = params.subjectName;
+params.numRuns = 10; 
+params.reconall = 0; 
+params.slicetiming = 0;
+params.refvol = 1;
+params.regFirst = 1;
+params.filtType = 'high';
+params.lowHz = 0.01;
+params.highHz = 0.10;
+params.physio = 1;
+params.motion = 1;
+params.task = 0;
+params.localWM = 1;
+params.anat = 1;
+params.amem = 20;
+params.fmem = 50;
+create_preprocessing_scripts(params);
 %% TOME_3002 - session 1
-session_dir = '/data/jag/TOME/TOME_3002/082616a';
-subject_name = 'TOME_3002'; % Freesurfer subject name (may not match job_name)
-outDir = fullfile(session_dir,'preprocessing_scripts');
-logDir = '/data/jet/abock/LOGS';
-job_name = 'TOME_3002'; % Name for this job/session (may not match subject_name)
-numRuns = 4; % number of bold runs
-reconall = 1;
-slicetiming = 0; % correct slice timings
-refvol = 1; % motion correct to 1st TR
-filtType = 'high';
-lowHz = 0.01;
-highHz = 0.10;
-physio = 1;
-motion = 1;
-task = 0;
-localWM = 1;
-anat = 1;
-amem = 20;
-fmem = 50;
+params.sessionDir = '/data/jag/TOME/TOME_3002/082616a';
+params.subjectName = 'TOME_3002';
+params.outDir = fullfile(params.sessionDir,'preprocessing_scripts');
+params.logDir = '/data/jet/abock/LOGS';
+params.jobName = params.subjectName;
+params.numRuns = 4; 
+params.reconall = 0; 
+params.slicetiming = 0;
+params.refvol = 1;
+params.regFirst = 1;
+params.filtType = 'high';
+params.lowHz = 0.01;
+params.highHz = 0.10;
+params.physio = 1;
+params.motion = 1;
+params.task = 0;
+params.localWM = 1;
+params.anat = 1;
+params.amem = 20;
+params.fmem = 50;
+create_preprocessing_scripts(params);
+%% TOME_3002 - session 2
 
-create_preprocessing_scripts(session_dir,subject_name,outDir,logDir,job_name...
-    ,numRuns,reconall,slicetiming,refvol,filtType,lowHz,highHz,physio,motion,task,localWM,anat,amem,fmem)
+%%% copy over the MPRAGE directory from TOME_3002 - session 1 %%%
+
+params.sessionDir = '/data/jag/TOME/TOME_3002/082616b';
+params.subjectName = 'TOME_3002';
+params.outDir = fullfile(params.sessionDir,'preprocessing_scripts');
+params.logDir = '/data/jet/abock/LOGS';
+params.jobName = params.subjectName;
+params.numRuns = 10; 
+params.reconall = 0; 
+params.slicetiming = 0;
+params.refvol = 1;
+params.regFirst = 1;
+params.filtType = 'high';
+params.lowHz = 0.01;
+params.highHz = 0.10;
+params.physio = 1;
+params.motion = 1;
+params.task = 0;
+params.localWM = 1;
+params.anat = 1;
+params.amem = 20;
+params.fmem = 50;
+create_preprocessing_scripts(params);
+%% TOME_3003 - session 1
+params.sessionDir = '/data/jag/TOME/TOME_3003/090216';
+params.subjectName = 'TOME_3003';
+params.outDir = fullfile(params.sessionDir,'preprocessing_scripts');
+params.logDir = '/data/jet/abock/LOGS';
+params.jobName = params.subjectName;
+params.numRuns = 4; 
+params.reconall = 1;
+params.slicetiming = 0;
+params.refvol = 1;
+params.regFirst = 1;
+params.filtType = 'high';
+params.lowHz = 0.01;
+params.highHz = 0.10;
+params.physio = 1;
+params.motion = 1;
+params.task = 0;
+params.localWM = 1;
+params.anat = 1;
+params.amem = 20;
+params.fmem = 50;
+create_preprocessing_scripts(params);
+%%
+
+
+
+
+%% TOME_3002 - project template
+session_dir = '/data/jag/TOME/TOME_3002/082616b';
+subject_name = 'TOME_3002'; % Freesurfer subject name (may not match job_name)
+project_template(session_dir,subject_name);
