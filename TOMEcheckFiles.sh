fdc85cfc9c2cdb3b0401e82816eb73c67e4949a7
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-09-23 10:32:02 -0400

    TOMEcheckFiles working
    To do: add more detailed checks.

--- TOMEcheckFiles.sh
+++ TOMEcheckFiles.sh
@@ -2,7 +2,7 @@
 #set -ex  #uncomment just for debugging
 
 echo "Which files do you want to check?"
-echo "1. All subject and sessions on the cluster (must be on the cluster)"
+echo "1. All subject and sessions on the cluster (must be on the cluster or have cluster mounted as /data/jag/...)"
 echo "2. All subject and sessions on Dropbox (must be on machine with read access to TOME_data)"
 read lookHere
 
@@ -11,61 +11,55 @@ case $lookHere in
 	echo "List of all existing subjects in /data/jag/TOME/ :"
 	ls -ltr /data/jag/TOME/TOME*
 	echo "Checking if all sessions contain all needed files..."
-	
-	### put a loop here
-	
-	for subjName in /data/jag/TOME/*; do
-		if [[ $subjName == "TOME"* ]]; then
-			for sessionDate in /data/jag/TOME/$subjName/*; do
-				if [ ! -d /data/jag/TOME/$subjName/$sessionDate/DICOMS ]; then
-					faultySession="/data/jag/TOME/$subjName/$sessionDate/"
-					echo "MISSING DICOMS DIRECTORY IN:"
-					echo $faultySession
+	subjs=(/data/jag/TOME/TOME*)
+	echo "${subjs[@]}"
+	for subjDir in "${subjs[@]}"; do
+		sessionDirs=("$subjDir"/*)
+		echo "Checking all existing session in $subjDir :" 
+		ls "$subjDir"/
+			for sessionDate in "${sessionDirs[@]}"; do
+				if [ ! -d "$sessionDate"/DICOMS ]; then
+					echo ">>> MISSING DICOMS DIRECTORY IN:"
+					echo "$sessionDate"
 				else
 					echo "..."
 				fi
 		
-				if [ ! -d /data/jag/TOME/$subjName/$sessionDate/PulseOx ]; then
-					faultySession="/data/jag/TOME/$subjName/$sessionDate/"
+				if [ ! -d "$sessionDate"/PulseOx ]; then
 					echo "MISSING PulseOx DIRECTORY IN:"
-					echo $faultySession
+					echo "$sessionDate"
 				else
 					echo "..."
 				fi
 		
-				if [ ! -d /data/jag/TOME/$subjName/$sessionDate/Protocols ]; then
-					faultySession="/data/jag/TOME/$subjName/$sessionDate/"
+				if [ ! -d "$sessionDate"/Protocols ]; then
 					echo "MISSING Protocols DIRECTORY IN:"
-					echo $faultySession
+					echo "$sessionDate"
 				else
 					echo "..."
 				fi
 		
-				if [ ! -f /data/jag/TOME/$subjName/$sessionDate/Protocols/coeff.grad ]; then
-					faultySession="/data/jag/TOME/$subjName/$sessionDate/"
+				if [ ! -f "$sessionDate"/Protocols/coeff.grad ]; then
 					echo "MISSING coeff.grad FILE IN:"
-					echo $faultySession
+					echo "$sessionDate"
 				else
 					echo "..."
 				fi
 			
-				if [ ! -d /data/jag/TOME/$subjName/$sessionDate/Stimuli ]; then
-					faultySession="/data/jag/TOME/$subjName/$sessionDate/"
+				if [ ! -d "$sessionDate"/Stimuli ]; then
 					echo "MISSING Stimuli DIRECTORY IN:"
-					echo $faultySession
+					echo "$sessionDate"
 				else
 					echo "..."
 				fi
 			
-				if [ ! -f /data/jag/TOME/$subjName/$sessionDate/README.md ]; then
-					faultySession="/data/jag/TOME/$subjName/$sessionDate/"
+				if [ ! -f "$sessionDate"/README.md ]; then
 					echo "MISSING README.md DOCUMENT IN:"
-					echo $faultySession
+					echo "$sessionDate"
 				else
 					echo "..."
 				fi
 			done
-		fi
 	done
 	echo "Check completed!"
 ;;
@@ -77,54 +71,49 @@ case $lookHere in
 		echo "List of all existing subjects in /Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/:"
 		ls /Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/
 		subjs=(/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/*)
-		echo $subjs
-		for subjName in "${subjs[@]}"; do
-			echo $subjName
-			sessionDirs=(/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/"$condition"/"$subjName"/*)
-			#if [[ subjName == "TOME"* ]]; then
+		for subjDir in "${subjs[@]}"; do
+			sessionDirs=("$subjDir"/*)
+			echo "Checking all existing session in $subjDir :" 
+			ls "$subjDir"/
 				for sessionDate in "${sessionDirs[@]}"; do
-				ls /Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/"$condition"/"$subjName"/
-				echo $sessionDate
-				echo "AAAAA"
-					if [ ! -d /Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/"$condition"/"$subjName"/"$sessionDate"/DICOMS ]; then
-						echo "MISSING DICOMS DIRECTORY IN:"
-						echo "/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/$subjName/$sessionDate/"
-						continue
+					if [ ! -d "$sessionDate"/ScannerFiles ]; then
+						echo ">>> MISSING ScannerFiles DIRECTORY IN:"
+						echo "$sessionDate"
+					else
+						echo "..."
+					fi
+					
+					if [ ! -d "$sessionDate"/ScannerFiles/Protocols ]; then
+						echo ">>> MISSING ScannerFiles/Protocols DIRECTORY IN:"
+						echo "$sessionDate/"
+					else
+						echo "..."
+					fi
+					
+					if [ ! -d "$sessionDate/Scan_Checklists" ]; then
+						echo ">>> MISSING ScanChecklists DIRECTORY IN:"
+						echo "$sessionDate/"
 					else
 						echo "..."
 					fi
 	
-					if [ ! -d "/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/$subjName/$sessionDate/ScannerFiles" ]; then
-						echo "MISSING ScannerFiles DIRECTORY IN:"
-						echo "/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/$subjName/$sessionDate/"
-					else
-						echo "..."
-					fi
-	
-					if [ ! -d "/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/$subjName/$sessionDate/Scan_Checklists" ]; then
-						echo "MISSING ScanChecklists DIRECTORY IN:"
-						echo "/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/$subjName/$sessionDate/"
-					else
-						echo "..."
-					fi
-	
-					if [ ! -d "/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/$subjName/$sessionDate/EyeTracking" ]; then
-						echo "MISSING EyeTracking DIRECTORY IN:"
-						echo "/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/$subjName/$sessionDate/"
+					if [ ! -d "$sessionDate/EyeTracking" ]; then
+						echo ">>> MISSING EyeTracking DIRECTORY IN:"
+						echo "$sessionDate/"
 					else
 						echo "..."
 					fi
 		
-					if [ ! -d "/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/$subjName/$sessionDate/Stimuli" ]; then
-						echo "MISSING Stimuli DIRECTORY IN:"
-						echo "/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/$subjName/$sessionDate/"
+					if [ ! -d "$sessionDate/Stimuli" ]; then
+						echo ">>> MISSING Stimuli DIRECTORY IN:"
+						echo "$sessionDate"
 					else
 						echo "..."
 					fi
 		
-					if [ ! -f "/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/$subjName/$sessionDate/README.md" ]; then
-						echo "MISSING README.md document IN:"
-						echo "/Users/$USER/Dropbox-Aguirre-Brainard-Lab/TOME_data/$condition/$subjName/$sessionDate/"
+					if [ ! -f "$sessionDate/README.md" ]; then
+						echo ">>> MISSING README.md document IN:"
+						echo "$sessionDate"
 					else
 						echo "..."
 					fi
