e75f5f02b7b1ba7c588c8d39a1c13d8722541ff1
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-09-27 11:34:09 -0400

    TomecheckFiles now looks into folders for specific files

--- TOMEcheckFiles.sh
+++ TOMEcheckFiles.sh
@@ -39,7 +39,31 @@ case $lookHere in
 					echo ">>> MISSING coeff.grad FILE IN:"
 					echo "$sessionDate"
 				fi
-			
+				
+				for files in "$sessionDate"/Protocols/*.pdf; do
+  					if [ ! -e "$files" ]; then
+    					echo ">>> MISSING pdf protocol FILE IN:"
+						echo "$sessionDate"
+    					break
+  					fi
+				done
+
+				for edxdir in "$sessionDate"/Protocols/*.edxdir; do
+  					if [ ! -e "$edxdir" ]; then
+    					echo ">>> MISSING edxdir DIRECTORY IN:"
+						echo "$sessionDate"
+    					break
+  					fi
+				done
+				
+				for files in "$sessionDate"/Protocols/*.edx; do
+  					if [ ! -e "$files" ]; then
+    					echo ">>> MISSING edx FILE IN:"
+						echo "$sessionDate"
+    					break
+  					fi
+				done
+							
 				if [ ! -d "$sessionDate"/Stimuli ]; then
 					echo ">>> MISSING Stimuli DIRECTORY IN:"
 					echo "$sessionDate"
@@ -67,51 +91,104 @@ case $lookHere in
 		subjs=(/Users/$USER/Dropbox\ \(Aguirre-Brainard\ Lab\)/TOME_data/$condition/*)
 		for subjDir in "${subjs[@]}"; do
 			sessionDirs=("$subjDir"/*)
-				for sessionDate in "${sessionDirs[@]}"; do
+			for sessionDate in "${sessionDirs[@]}"; do
 				echo "   "
 				echo "   "
 				echo "Checking files in $sessionDate ..."
-					if [ ! -d "$sessionDate"/ScannerFiles ]; then
-						echo ">>> MISSING ScannerFiles DIRECTORY IN:"
+				if [ ! -d "$sessionDate"/ScannerFiles ]; then
+					echo ">>> MISSING ScannerFiles DIRECTORY IN:"
+					echo "$sessionDate"
+				fi
+				
+				if [ ! -d "$sessionDate"/ScannerFiles/Protocols ]; then
+					echo ">>> MISSING ScannerFiles/Protocols DIRECTORY IN:"
+					echo "$sessionDate/"
+				fi
+				
+				for edxdir in "$sessionDate"/ScannerFiles/Protocols/*.edxdir; do
+					if [ ! -e "$edxdir" ]; then
+						echo ">>> MISSING ScannerFiles/Protocols/edxdir DIRECTORY IN:"
 						echo "$sessionDate"
-					fi
-					
-					if [ ! -d "$sessionDate"/ScannerFiles/Protocols ]; then
-						echo ">>> MISSING ScannerFiles/Protocols DIRECTORY IN:"
-						echo "$sessionDate/"
-					fi
-					
-					if [ ! -d "$sessionDate"/ScannerFiles/PulseOx ]; then
-						echo ">>> MISSING ScannerFiles/PulseOx DIRECTORY IN:"
-						echo "$sessionDate/"
-					fi
-
-					if [ ! -f "$sessionDate"/ScannerFiles/coeff.grad ]; then
-						echo ">>> MISSING ScannerFiles/coeff.grad FILE IN:"
-						echo "$sessionDate/"
-					fi
-										
-					if [ ! -d "$sessionDate/Scan_Checklists" ]; then
-						echo ">>> MISSING ScanChecklists DIRECTORY IN:"
-						echo "$sessionDate/"
-					fi
-	
-					if [ ! -d "$sessionDate/EyeTracking" ]; then
-						echo ">>> MISSING EyeTracking DIRECTORY IN:"
-						echo "$sessionDate/"
-					fi
-		
-					if [ ! -d "$sessionDate/Stimuli" ]; then
-						echo ">>> MISSING Stimuli DIRECTORY IN:"
-						echo "$sessionDate"
-					fi
-		
-					if [ ! -f "$sessionDate/README.md" ]; then
-						echo ">>> MISSING README.md document IN:"
-						echo "$sessionDate"
+						break
 					fi
 				done
-			#fi
+				
+				for files in "$sessionDate"/ScannerFiles/Protocols/*.edx; do
+					if [ ! -e "$files" ]; then
+						echo ">>> MISSING ScannerFiles/Protocols/edx FILE IN:"
+						echo "$sessionDate/"
+						break
+					fi
+				done
+
+				for files in "$sessionDate"/ScannerFiles/Protocols/*.pdf; do
+					if [ ! -e "$files" ]; then
+						echo ">>> MISSING ScannerFiles/Protocols/pdf FILE IN:"
+						echo "$sessionDate/"
+						break
+					fi
+				done					
+				
+				if [ ! -d "$sessionDate"/ScannerFiles/PulseOx ]; then
+					echo ">>> MISSING ScannerFiles/PulseOx DIRECTORY IN:"
+					echo "$sessionDate/"
+				fi
+
+				if [ ! -f "$sessionDate"/ScannerFiles/coeff.grad ]; then
+					echo ">>> MISSING ScannerFiles/coeff.grad FILE IN:"
+					echo "$sessionDate/"
+				fi
+									
+				if [ ! -d "$sessionDate/ScanChecklists" ]; then
+					echo ">>> MISSING ScanChecklists DIRECTORY IN:"
+					echo "$sessionDate/"
+				fi
+				
+				
+				for files in "$sessionDate"/ScanChecklists/HCLV-*Checklist.pdf; do
+					if [ ! -e "$files" ]; then
+						echo ">>> MISSING Checklist.pdf DOCUMENT IN:"
+						echo "$sessionDate/"
+						break
+					fi
+				done	
+									
+				if [ ! -f "$sessionDate"/ScanChecklists/HCLV-dataCollection.pdf ]; then
+					echo ">>> MISSING HCLV-dataCollection.pdf DOCUMENT IN:"
+					echo "$sessionDate/"
+				fi
+
+				if [ ! -d "$sessionDate"/EyeTracking ]; then
+					echo ">>> MISSING EyeTracking DIRECTORY IN:"
+					echo "$sessionDate/"
+				fi
+				
+				for files in "$sessionDate"/EyeTracking/*; do
+					if [ ! -e "$files" ]; then
+						echo ">>> MISSING EyeTracking FILES IN (directory is empty):"
+						echo "$sessionDate/"
+						break
+					fi
+				done
+	
+				if [ ! -d "$sessionDate/Stimuli" ]; then
+					echo ">>> MISSING Stimuli DIRECTORY IN:"
+					echo "$sessionDate"
+				fi
+				
+				for files in "$sessionDate"/Stimuli/*; do
+					if [ ! -e "$files" ]; then
+						echo ">>> MISSING Stimuli FILES IN (directory is empty):"
+						echo "$sessionDate"
+						break
+					fi
+				done
+	
+				if [ ! -f "$sessionDate/README.md" ]; then
+					echo ">>> MISSING README.md document IN:"
+					echo "$sessionDate"
+				fi
+			done
 		done
 	done
 	echo "   "
