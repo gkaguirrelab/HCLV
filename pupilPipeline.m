f72c07689740228dcaea3ae24608311b57c6a3c3
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-11-18 09:52:57 -0500

    [NF] added pupilPipeline.m for cluster pupil tracking

---
+++ pupilPipeline.m
@@ -0,0 +1,52 @@
+function pupilPipeline (params, mainDir)
+% This function is a wrapper to run the full pupilTrack pipeline for a
+% single video. 
+% Note: contains internal params for each step where required.
+
+%  Usage:
+% %% Set cluster directory
+% mainDir= '/data/jag';
+% %% Set initial params
+% % run params
+% params.subjectName = 'TOME_3000';
+% params.sessionDate = '103116';
+% params.runName = 'tfMRI_FLASH_AP_run01';
+% 
+% % project params (fixed)
+% params.outputDir = 'TOME';
+% params.projectFolder = 'TOME';
+% params.eyeTrackingDir = 'EyeTracking';
+% 
+% pupilPipeline (params, mainDir)
+
+%% set outDir
+outDir = fullfile(mainDir,params.outputDir, params.projectSubfolder, ...
+    params.subjectName,params.sessionDate,params.eyeTrackingDir);
+
+%% deinterlace
+deinterlaceVideo (params, mainDir);
+
+%% track with pupilTrack
+params.acqRate = 60;
+params.inVideo = fullfile(outDir,[params.runName '_60hz.avi']);
+params.outVideo = fullfile(outDir,[params.runName '_pupilTrack.avi']);
+params.outMat = fullfile(outDir, [params.runName '_pupilTrack.mat']);
+
+trackPupil(params);
+
+%% get timeBase and save it
+params.ltRes = [360 240]; % resolution of the LiveTrack video (half original size)
+params.ptRes = [400 300]; % resolution of the pupilTrack video
+params.ltThr = 0.1; % threshold for liveTrack glint position
+params.pupilTrackFile = params.outMat;
+
+[timeBase] = getPupilTimebase(params,mainDir);
+save(fullfile(outDir,[params.runName '_timeBase.mat']), 'timeBase');
+
+%% Save params for this analysis
+save(fullfile(outDir, [params.runName '_params.mat']), 'params');
+
+%% Remove the 60Hz video (it is no longer needed)
+delete(params.inVideo);
+
+
