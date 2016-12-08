561c0d8a6a267ef10296b6b5daafe748eeb240f6
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-12-08 17:10:11 -0500

    [NF] added new scripts

---
+++ makePupilPlots.m
@@ -0,0 +1,282 @@
+% script to obtain several plots
+
+%% loop in the responseArray to find and bin all runs for each session
+
+% get total frames
+nTR = 420;
+TRlength = 0.8; %[sec]
+acqRate = 60; %[Hz]
+framesPerTR = TRlength/(1/acqRate);
+totalFrames = nTR * framesPerTR;
+
+
+nSessTypes = size(responseArray,1);
+nSubjects = size(responseArray,2);
+nSessions = size(responseArray,3);
+nRuns = size(responseArray,4);
+
+restCT = 1;
+movieCT = 1;
+retinoCT = 1;
+flashCT = 1;
+for st=1:nSessTypes
+    for sj=1:nSubjects
+        for ss=1:nSessions
+            for rr=1:nRuns
+                if ~isempty(responseArray{st,sj,ss,rr})
+                    % bin the runs based on their name
+                    if regexp(responseArray{st,sj,ss,rr,1}{4},regexptranslate('wildcard','*REST*'))
+                        % find zero in timebase
+                        zeroFrame = find (~responseArray{st,sj,ss,rr,1}{1,5}.timeBase);
+                        if isempty(zeroFrame)
+                            warning(['TimeBase problem for ' responseArray{st,sj,ss,rr,1}{1,5}.metadata.subjectName responseArray{st,sj,ss,rr,1}{1,5}.metadata.runName ])
+                        else
+                            % trim the response
+                            rest{sj,restCT} = responseArray{st,sj,ss,rr,1}{1,5}.pupilSize(zeroFrame : zeroFrame + totalFrames);
+                            restCT = restCT+1;
+                        end
+                    elseif regexp(responseArray{st,sj,ss,rr,1}{4},regexptranslate('wildcard','*MOVIE*'))
+                        % find zero in timebase
+                        zeroFrame = find (~responseArray{st,sj,ss,rr,1}{1,5}.timeBase);
+                        if isempty(zeroFrame)
+                            warning(['TimeBase problem for ' responseArray{st,sj,ss,rr,1}{1,5}.metadata.subjectName responseArray{st,sj,ss,rr,1}{1,5}.metadata.runName ])
+                        else
+                            % trim the response
+                            movie{sj,movieCT} = responseArray{st,sj,ss,rr,1}{1,5}.pupilSize(zeroFrame : zeroFrame + totalFrames);
+                            movieCT = movieCT+1;
+                        end
+                    elseif regexp(responseArray{st,sj,ss,rr,1}{4},regexptranslate('wildcard','*RETINO*'))
+                        % find zero in timebase
+                        zeroFrame = find (~responseArray{st,sj,ss,rr,1}{1,5}.timeBase);
+                        if isempty(zeroFrame)
+                            warning(['TimeBase problem for ' responseArray{st,sj,ss,rr,1}{1,5}.metadata.subjectName responseArray{st,sj,ss,rr,1}{1,5}.metadata.runName ])
+                        else
+                            % trim the response
+                            retino{sj,retinoCT} = responseArray{st,sj,ss,rr,1}{1,5}.pupilSize(zeroFrame : zeroFrame + totalFrames);
+                            retinoCT = retinoCT+1;
+                        end
+                    elseif regexp(responseArray{st,sj,ss,rr,1}{4},regexptranslate('wildcard','*FLASH*'))
+                        % find zero in timebase
+                        zeroFrame = find (~responseArray{st,sj,ss,rr,1}{1,5}.timeBase);
+                        if isempty(zeroFrame)
+                            warning(['TimeBase problem for ' responseArray{st,sj,ss,rr,1}{1,5}.metadata.subjectName responseArray{st,sj,ss,rr,1}{1,5}.metadata.runName ])
+                        else
+                            % trim the response
+                            flash{sj,flashCT} = responseArray{st,sj,ss,rr,1}{1,5}.pupilSize(zeroFrame : zeroFrame + totalFrames);
+                            flashCT = flashCT+1;
+                        end
+                    end
+                end
+            end
+        end
+        % reset the counter for the new subject
+        restCT = 1;
+        movieCT = 1;
+        retinoCT = 1;
+        flashCT = 1;
+    end
+end
+
+%% calculate and plot the median and the std for every subject
+for sj=1:nSubjects
+    if ~isempty(responseArray{1,sj})
+        restMedian{sj} = nanmedian([rest{sj,:}]);
+        restStd{sj} = nanstd([rest{sj,:}]);
+        xtickrest{sj} = responseArray{1,sj}{2};
+    end
+    if ~isempty(responseArray{2,sj})
+        movieMedian{sj} = nanmedian([movie{sj,:}]);
+        movieStd{sj} = nanstd([movie{sj,:}]);
+        retinoMedian{sj} = nanmedian([retino{sj,:}]);
+        retinoStd{sj} = nanstd([retino{sj,:}]);
+        flashMedian{sj} = nanmedian([flash{sj,:}]);
+        flashStd{sj} = nanstd([flash{sj,:}]);
+        xtickTwo{sj} = responseArray{1,sj}{2};
+    end
+end
+
+h = fullFigure;
+YMIN = 0;
+YMAX = 10;
+titleSize = 16;
+
+subplot(2,2,1)
+bar(cell2mat(restMedian),'FaceColor',[0.4 0.6 0.8])
+hold on
+errorbar(cell2mat(restMedian),cell2mat(restStd),'k.')
+title (['RESTING STATE (median ' setstr(177) ' SD)'],'FontSize', titleSize)
+ylabel('Pupil diameter [mm]')
+ax = gca;
+set(gca,'TickLabelInterpreter','none')
+ax.XTickLabel = ([xtickrest]);
+ax.XTickLabelRotation = 45;
+ylim([YMIN YMAX])
+
+subplot(2,2,2)
+bar(cell2mat(retinoMedian),'FaceColor',[0.4 0.6 0.8])
+hold on
+errorbar(cell2mat(retinoMedian),cell2mat(retinoStd),'k.')
+title (['RETINO (median ' setstr(177) ' SD)'],'FontSize',titleSize)
+ylabel('Pupil diameter [mm]')
+ylabel('Pupil diameter [mm]')
+ax = gca;
+set(gca,'TickLabelInterpreter','none')
+ax.XTickLabel = ([xtickTwo]);
+ax.XTickLabelRotation = 45;
+ylim([YMIN YMAX])
+
+subplot(2,2,3)
+bar(cell2mat(movieMedian),'FaceColor',[0.4 0.6 0.8])
+hold on
+errorbar(cell2mat(movieMedian),cell2mat(movieStd),'k.')
+title (['MOVIE (median ' setstr(177) ' SD)'],'FontSize',titleSize)
+ylabel('Pupil diameter [mm]')
+ylabel('Pupil diameter [mm]')
+ax = gca;
+set(gca,'TickLabelInterpreter','none')
+ax.XTickLabel = ([xtickTwo]);
+ax.XTickLabelRotation = 45;
+ylim([YMIN YMAX])
+
+subplot(2,2,4)
+bar(cell2mat(flashMedian),'FaceColor',[0.4 0.6 0.8])
+hold on
+errorbar(cell2mat(flashMedian),cell2mat(flashStd),'k.')
+title (['FLASH (median ' setstr(177) ' SD)'],'FontSize',titleSize)
+ylabel('Pupil diameter [mm]')
+ylabel('Pupil diameter [mm]')
+ax = gca;
+set(gca,'TickLabelInterpreter','none')
+ax.XTickLabel = ([xtickTwo]);
+ax.XTickLabelRotation = 45;
+ylim([YMIN YMAX])
+
+%adjustments
+axesHandles = findobj(h, 'type', 'axes');
+set(axesHandles, 'TickDir', 'out');
+set(h, 'PaperPosition', [0 0 9 9]);
+set(h, 'PaperSize', [9 9]);
+
+saveas(h, '/Users/giulia/Dropbox-Aguirre-Brainard-Lab/TOME_analysis/medianPupilSize', 'pdf') %Save figure
+close h
+
+%% plot separately each REST run
+for sj=1:nSubjects
+    h = fullFigure;
+    plot([rest{sj,1}]);hold on;
+    plot([rest{sj,2}]);hold on;
+    plot([rest{sj,3}]);hold on;
+    plot([rest{sj,4}]);hold on;
+    ylim ([0 10])
+    legend('Run01', 'Run02' , 'Run03' , 'Run04')
+    xlim ([0 20161])
+    ax = gca;
+    ax.XTick = [0:framesPerTR*10:totalFrames];
+    ax.XTick = [0:framesPerTR*20:totalFrames];
+    ax.XTickLabel = [0:20:420];
+    ax.XTickLabel = [1:20:420];
+    ax.XTickLabel = [1:20-1:420];
+    ax.XTickLabel = [0:20:420];
+    title (['Resting state timeseries - ' responseArray{1,sj}{2}], 'Interpreter' , 'none')
+    xlabel ('TR')
+    ylabel('Pupil diameter [mm]')
+    
+    %adjustments
+    axesHandles = findobj(h, 'type', 'axes');
+    set(axesHandles, 'TickDir', 'out');
+    set(h, 'PaperPosition', [0 0 9 9]);
+    set(h, 'PaperSize', [9 9]);
+    
+    saveas(h, fullfile('/Users/giulia/Desktop/TEST/', [responseArray{1,sj}{2} '_restStateTimeserie']), 'pdf') %Save figure
+    close all
+end
+%% MOVIE
+for sj=1:nSubjects
+    h = fullFigure;
+    plot([movie{sj,1}]);hold on;
+    plot([movie{sj,2}]);hold on;
+    plot([movie{sj,3}]);hold on;
+    plot([movie{sj,4}]);hold on;
+    ylim ([0 10])
+    legend('Run01', 'Run02' , 'Run03' , 'Run04')
+    xlim ([0 20161])
+    ax = gca;
+    ax.XTick = [0:framesPerTR*10:totalFrames];
+    ax.XTick = [0:framesPerTR*20:totalFrames];
+    ax.XTickLabel = [0:20:420];
+    ax.XTickLabel = [1:20:420];
+    ax.XTickLabel = [1:20-1:420];
+    ax.XTickLabel = [0:20:420];
+    title (['Movie runs timeseries - ' responseArray{1,sj}{2}], 'Interpreter' , 'none')
+    xlabel ('TR')
+    ylabel('Pupil diameter [mm]')
+    
+    %adjustments
+    axesHandles = findobj(h, 'type', 'axes');
+    set(axesHandles, 'TickDir', 'out');
+    set(h, 'PaperPosition', [0 0 9 9]);
+    set(h, 'PaperSize', [9 9]);
+    
+    saveas(h, fullfile('/Users/giulia/Desktop/TEST/', [responseArray{1,sj}{2} '_movieTimeserie']), 'pdf') %Save figure
+    close all
+end
+
+%% retino
+for sj=1:nSubjects
+    h = fullFigure;
+    plot([retino{sj,1}]);hold on;
+    plot([retino{sj,2}]);hold on;
+    plot([retino{sj,3}]);hold on;
+    plot([retino{sj,4}]);hold on;
+    ylim ([0 10])
+    legend('Run01', 'Run02' , 'Run03' , 'Run04')
+    xlim ([0 20161])
+    ax = gca;
+    ax.XTick = [0:framesPerTR*10:totalFrames];
+    ax.XTick = [0:framesPerTR*20:totalFrames];
+    ax.XTickLabel = [0:20:420];
+    ax.XTickLabel = [1:20:420];
+    ax.XTickLabel = [1:20-1:420];
+    ax.XTickLabel = [0:20:420];
+    title (['Retino runs timeseries - ' responseArray{1,sj}{2}], 'Interpreter' , 'none')
+    xlabel ('TR')
+    ylabel('Pupil diameter [mm]')
+    
+    %adjustments
+    axesHandles = findobj(h, 'type', 'axes');
+    set(axesHandles, 'TickDir', 'out');
+    set(h, 'PaperPosition', [0 0 9 9]);
+    set(h, 'PaperSize', [9 9]);
+    
+    saveas(h, fullfile('/Users/giulia/Desktop/TEST/', [responseArray{1,sj}{2} '_retinoTimeserie']), 'pdf') %Save figure
+    close all
+end
+
+%% flash
+for sj=1:nSubjects
+    h = fullFigure;
+    plot([flash{sj,1}]);hold on;
+    plot([flash{sj,2}]);hold on;
+    ylim ([0 10])
+    legend('Run01', 'Run02')
+    xlim ([0 20161])
+    ax = gca;
+    ax.XTick = [0:framesPerTR*10:totalFrames];
+    ax.XTick = [0:framesPerTR*20:totalFrames];
+    ax.XTickLabel = [0:20:420];
+    ax.XTickLabel = [1:20:420];
+    ax.XTickLabel = [1:20-1:420];
+    ax.XTickLabel = [0:20:420];
+    title (['Flash runs timeseries - ' responseArray{1,sj}{2}], 'Interpreter' , 'none')
+    xlabel ('TR')
+    ylabel('Pupil diameter [mm]')
+    
+    %adjustments
+    axesHandles = findobj(h, 'type', 'axes');
+    set(axesHandles, 'TickDir', 'out');
+    set(h, 'PaperPosition', [0 0 9 9]);
+    set(h, 'PaperSize', [9 9]);
+    
+    saveas(h, fullfile('/Users/giulia/Desktop/TEST/', [responseArray{1,sj}{2} '_flashTimeserie']), 'pdf') %Save figure
+    close all
+end
---
+++ makeResponseArray.m
@@ -0,0 +1,53 @@
+function [responseArray] = makeResponseArray(params,dropboxDir)
+
+% Get user name and define path to dropbox folder
+[~, tmpName] = system('whoami');
+userName = strtrim(tmpName);
+dropboxDir = fullfile('/Users', userName, '/Dropbox-Aguirre-Brainard-Lab');
+
+% project paramsRe
+params.outputDir = 'CumulativeAnalyses';
+params.projectFolder = 'TOME_analysis';
+params.projectSubfolder = 'session*';
+params.subjNaming = 'TOME_3*';
+params.eyeTrackingDir = 'EyeTracking';
+
+sessions = dir(fullfile(dropboxDir, params.projectFolder, params.projectSubfolder));
+for cc = 1 : length(sessions) % loop in sessions
+
+ % look for subjects in each session
+    subjects = dir(fullfile(dropboxDir, params.projectFolder, sessions(cc).name, params.subjNaming));
+    if isempty(subjects)
+        continue
+    else
+        for ss = 1 : length(subjects) % loop in subjects
+            % look for dates in each subject and check if folder is valid
+            datesTMP = dir(fullfile(dropboxDir, params.projectFolder, sessions(cc).name, subjects(ss).name));
+            cnt = 1;
+            for jj = 1:length(datesTMP) % check if valid
+                if datesTMP(jj).isdir && ~strcmp(datesTMP(jj).name,'.') && ~strcmp(datesTMP(jj).name,'..')
+                    dates(cnt) = datesTMP(jj);
+                    cnt = cnt +1;
+                end
+            end
+            for dd = 1:length(dates) % loop in dates
+                 % look for runs in each date
+                runs = dir(fullfile(dropboxDir, params.projectFolder, sessions(cc).name, subjects(ss).name,dates(dd).name,params.eyeTrackingDir,'*response.mat'));
+                for rr = 1 :length(runs) %loop in runs
+                    % add fields in reportToProcess
+                    load(fullfile(dropboxDir, params.projectFolder,sessions(cc).name, subjects(ss).name, dates(dd).name, params.eyeTrackingDir,runs(rr).name))
+                    responseArray{cc,ss,dd,rr,1} = {sessions(cc).name, subjects(ss).name, dates(dd).name, runs(rr).name,response};
+                    clear response
+                end
+            end
+        end
+    end
+    clear subjects
+    clear dates
+    clear datesTMP
+    clear runs
+end
+
+% save respons array
+save (fullfile(dropboxDir, params.projectFolder,'responseArray.mat'),'responseArray')
+                    
